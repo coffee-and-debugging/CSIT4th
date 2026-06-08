193744cb1fa862949cf63c7e9d7d315c4ffe46b4
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2026-06-08 11:49:27 +0545

    feat: adding blogs model to the project

--- blog/content/models.py
+++ blog/content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    title= models.CharField(max_length=50)
+    subtitle= models.CharField(max_length=100)
+    image= models.ImageField(upload_to="images/")
+    description= models.TextField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    author= models.ForeignKey(User, on_delete=models.CASCADE, default=1)
