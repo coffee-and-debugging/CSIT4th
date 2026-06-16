8606f0b447bcecf25abd5a45db526ffa03d03df7
UNIQUE ADHIKARI <adhikariunique56@gmail.com>
2026-06-16 11:48:14 +0545

    chore: daily update

--- blog/content/views/main_view.py
+++ blog/content/views/main_view.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Blog
 
 def home(request):
     return render(request, "main/home.html")
 
 def create_blog(request):
+    if request.method== "POST":
+        title= request.POST.get("title")
+        subtitle= request.POST.get("subtitle")
+        description= request.POST.get("description")
+
+        blog= Blog(title=title, subtitle=subtitle, description=description)
+        blog.save()
+
     return render(request, "main/create_blog.html")
+
+
 
 def edit_blog(request):
     return render(request, "main/edit_blog.html")
